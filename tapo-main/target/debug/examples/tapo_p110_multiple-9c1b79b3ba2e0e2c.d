0704bdefedd33aa5d91f462aefc3faedc71bedfd
Adrián <72648802+Adri-Sanchez-Miguel@users.noreply.github.com>
2023-10-02 15:19:23 +0200

    More files

---
+++ tapo-main/target/debug/examples/tapo_p110_multiple-9c1b79b3ba2e0e2c.d
@@ -0,0 +1,5 @@
+/home/adrian/Descargas/Practicas/tapo-main/target/debug/examples/tapo_p110_multiple-9c1b79b3ba2e0e2c: examples/tapo_p110_multiple.rs
+
+/home/adrian/Descargas/Practicas/tapo-main/target/debug/examples/tapo_p110_multiple-9c1b79b3ba2e0e2c.d: examples/tapo_p110_multiple.rs
+
+examples/tapo_p110_multiple.rs:
---
+++ tapo-main/target/debug/examples/tapo_p110_multiple-c3403366f61bbfce.d
@@ -0,0 +1,5 @@
+/home/adrian/Descargas/Practicas/tapo-main/target/debug/examples/tapo_p110_multiple-c3403366f61bbfce: examples/tapo_p110_multiple.rs
+
+/home/adrian/Descargas/Practicas/tapo-main/target/debug/examples/tapo_p110_multiple-c3403366f61bbfce.d: examples/tapo_p110_multiple.rs
+
+examples/tapo_p110_multiple.rs:
