2480c02251a426bce6fd56199cde803964c426d0
Adrián <72648802+Adri-Sanchez-Miguel@users.noreply.github.com>
2024-01-16 13:05:43 +0100

    Binaries for code execution in other raspberries

---
+++ tapo-main/target/examples/tapo_p110_multiple-dd6bcc6f7960fe3e.d
@@ -0,0 +1,5 @@
+/home/adrian/Downloads/tapo-main/target/release/examples/tapo_p110_multiple-dd6bcc6f7960fe3e: examples/tapo_p110_multiple.rs
+
+/home/adrian/Downloads/tapo-main/target/release/examples/tapo_p110_multiple-dd6bcc6f7960fe3e.d: examples/tapo_p110_multiple.rs
+
+examples/tapo_p110_multiple.rs:
